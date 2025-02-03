4cd290fde6922e6f2072ef3eab86c33646fb0a70
Rafael <rafaelwayv@gmail.com>
2025-02-03 16:22:53 -0300

    add todo-list project

--- src/app/portfolio/components/projects/projects.component.ts
+++ src/app/portfolio/components/projects/projects.component.ts
@@ -28,7 +28,7 @@ export class ProjectsComponent {
       title: 'Calculadora',
       width: '80px',
       height: '80px',
-      description: `Teste  a calculadora feita por mim, do zero, usando Angular e suas funcionalidades!
+      description: `Teste a calculadora feita por mim, do zero, usando Angular e suas funcionalidades!
         <br>
         <br>
         Ela <b>ainda</b> é uma calculadora simples, com todas as operações básicas, mas feita com muito carinho e dedicação.  `,
@@ -41,15 +41,18 @@ export class ProjectsComponent {
     },
     {
       src: 'img/projects/todo-list.png',
-      alt: 'Projeto da Lista de tarefas',
-      title: 'To-do List',
+      alt: 'Projeto da Lista de Tarefas',
+      title: 'Lista de tarefas',
       width: '80px',
       height: '80px',
-      description: `(WIP) Um projeto futuro, mas bem interessante e completo! Utilizando, obviamente, angular e suas funcionalidades.`,
+      description: `Bem vindo à minha lista de tarefas!
+      <br>
+      <br>
+      Feita inteiramente com Angular 19 e suas funcionalidades, explore e gerencie sua própria lista de tarefas. Adicione, delete, mude, marque como pendente, ou concluída cada uma de suas tarefas da maneira que quiser!`,
       links: [
         {
-          name: 'WIP',
-          href: '',
+          name: 'Explore a lista de tarefas',
+          href: 'https://cyberafa.github.io/todo-list/',
         },
       ],
     },
